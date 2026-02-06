215c04bb455930dfb43a1be0eae2b730e36c51d6
yeahbrahh <yeahbrahh@proton.me>
2026-02-05 17:53:41 -0700

    Parsed all data

--- project/code/scheduler.py
+++ project/code/scheduler.py
@@ -2,18 +2,30 @@ from rdflib import Graph
 from rdflib import URIRef
 from rdflib.namespace import split_uri
 
+from tkinter import *
+
+ROOMPREFIX = "http://example.org/Room"
+SLOTPREFIX = "http://example.org/_Time_slot"
+CAP_PREFIX = "http://example.org/roomCapacity"
+DUR_PREFIX = "http://example.org/examDuration"
+AVL_PREFIX = "http://example.org/hasAvailability"
+STARTPREFIX = "http://example.org/availableFrom"
+ENDPREFIX = "http://example.org/availableUntil"
+
 class_graph = Graph()
 room_graph = Graph()
 student_graph = Graph()
 
 class_graph.parse("./data/classes.ttl", format="turtle")
-room_graph.parse("./data/rooms.ttl")
-student_graph.parse("./data/students.ttl")
+room_graph.parse("./data/rooms.ttl", format="turtle")
+student_graph.parse("./data/students.ttl", format="turtle")
 
-student_courses = {}
 classes = {}
+student_courses = {}
+room_caps_and_availability = {}
+time_slot_defs = {}
 
-# get all classes a student is enrolled in
+# get all the students and the classes they are enrolled in
 for student in student_graph.subjects():
     courses = []
     
@@ -22,28 +34,60 @@ for student in student_graph.subjects():
          courses.append(course_code)
     if courses:
         _, student_name = split_uri(student)
+        student_name = student_name[1:]
         student_courses[student_name] = courses                         
 
 for student in student_courses.items():
     print(student)
 
-for k, v in student_courses.items():
-    print(k, v)
-
-tup = ()
-
-# gets min room capacities and exam durations (respectively)
+# gets all classes and their min room capacities and exam durations (respectively)
 for c in class_graph.subjects():
-    room_cap = next(class_graph.objects(subject=c, predicate=URIRef("http://example.org/hasMinimumRoomCapacity")), None)
-    exam_duration = next(class_graph.objects(subject=c, predicate=URIRef("http://example.org/examDuration")), None)
+    min_room_cap = next(class_graph.objects(subject=c, predicate=URIRef(CAP_PREFIX)), None)
+    exam_duration = next(class_graph.objects(subject=c, predicate=URIRef(DUR_PREFIX)), None)
 
-    if room_cap and exam_duration:
+    if min_room_cap and exam_duration:
         _, class_code = split_uri(c)
-        classes[class_code] = (room_cap.toPython(), float(exam_duration.toPython()))
+        classes[class_code] = (min_room_cap.toPython(), float(exam_duration.toPython()))
 
 
 for c in classes.items():
     print()
     print(c)
 
-    print(type(classes["CS310"][0]))
+# get the available rooms plus their capacity and available time slots
+for subject in room_graph.subjects():
+    if subject.startswith(ROOMPREFIX):
+        _, class_room = split_uri(subject)
+        room_cap = next(room_graph.objects(subject=subject, predicate=URIRef(CAP_PREFIX)), None)
+        temp = list(room_graph.objects(subject=subject, predicate=URIRef(AVL_PREFIX)))
+        available_slots = []
+        for p in temp:
+            _, slot = split_uri(p)
+            slot = slot[1:]
+            available_slots.append(slot)
+        room_caps_and_availability[class_room] = (int(room_cap.toPython()), available_slots)
+
+for r in room_caps_and_availability.items():
+    print()
+    print(r)
+
+
+## define the slots
+for subject in room_graph.subjects():
+    if subject.startswith(SLOTPREFIX):
+        _, slot = split_uri(subject)
+        slot = slot[1:]
+        start = next(room_graph.objects(subject=subject, predicate=URIRef(STARTPREFIX)), None)
+        end = next(room_graph.objects(subject=subject, predicate=URIRef(ENDPREFIX)), None)
+        
+        times = (start.toPython(), end.toPython())
+        time_slot_defs[slot] = times
+
+# print(times)
+
+for item in time_slot_defs.items():
+    print()
+    print(item)
+
+# for s, p, o in room_graph:
+#     print(s, p, o)
