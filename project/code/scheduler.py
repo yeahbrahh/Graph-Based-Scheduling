b0cd86e8f28691fcf9a5cc629cfd8e24df440a87
yeahbrahh <yeahbrahh@proton.me>
2026-02-15 12:37:37 -0700

    Got all perms for each exam within each time slot

--- project/code/scheduler.py
+++ project/code/scheduler.py
@@ -1,16 +1,12 @@
-from rdflib import Graph
-from rdflib import URIRef
+import verify
+
+from datetime import timedelta
+from rdflib import Graph, Namespace, URIRef
 from rdflib.namespace import split_uri
 
-ENROLLEDPRE = "http://example.org/enrolledIn"
-ROOMPRE = "http://example.org/Room"
-ROOMCAP_PRE = "http://example.org/roomCapacity"
-SLOTPRE = "http://example.org/_Time_slot"
-CAP_PRE = "http://example.org/hasMinimumRoomCapacity"
-DUR_PRE = "http://example.org/examDuration"
-AVL_PRE = "http://example.org/hasAvailability"
-STARTPRE = "http://example.org/availableFrom"
-ENDPRE = "http://example.org/availableUntil"
+EX = Namespace("http://example.org/")
+SCHEMA = Namespace("http://schema.org/")
+RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 
 class_graph = Graph()
 room_graph = Graph()
@@ -19,18 +15,19 @@ student_graph = Graph()
 class_graph.parse("./data/classes.ttl", format="turtle")
 room_graph.parse("./data/rooms.ttl", format="turtle")
 student_graph.parse("./data/students.ttl", format="turtle")
+
 classes = {}
 student_courses = {}
-room_caps_and_availability = {}
+min_room_caps_and_availability = {}
 time_slot_defs = {}
 
 # get all the students and the classes they are enrolled in
 for student in student_graph.subjects():
     courses = []
 
-    for course in student_graph.objects(subject=student, predicate=URIRef(ENROLLEDPRE)):
-         _, course_code = split_uri(course)
-         courses.append(course_code)
+    for course in student_graph.objects(subject=student, predicate=URIRef(EX.enrolledIn)):
+        _, course_code = split_uri(course)
+        courses.append(course_code)
     if courses:
         _, student_name = split_uri(student)
         student_name = student_name[1:]
@@ -42,9 +39,10 @@ for student in student_courses.items():
 # gets all classes and their min room capacities and exam durations (respectively)
 for c in class_graph.subjects():
     _, class_code = split_uri(c)
-    min_room_cap = class_graph.value(subject=c, predicate=URIRef(CAP_PRE))
-    exam_duration = class_graph.value(subject=c, predicate=URIRef(DUR_PRE))
-    classes[class_code] = (int(min_room_cap.toPython()), float(exam_duration.toPython()))
+    min_room_cap = class_graph.value(subject=c, predicate=URIRef(EX.hasMinimumRoomCapacity))
+    exam_duration = class_graph.value(subject=c, predicate=URIRef(EX.examDuration))
+    classes[class_code] = (int(min_room_cap.toPython()),
+                           float(exam_duration.toPython()))
 
 for c in classes.items():
     print()
@@ -53,18 +51,21 @@ for c in classes.items():
 
 # get the available rooms plus their capacity and available time slots
 for subject in room_graph.subjects():
-    if subject.startswith(ROOMPRE):
+    if subject.startswith(EX.roomCapacity):
         _, class_room = split_uri(subject)
-        room_cap = room_graph.value(subject=subject, predicate=URIRef(ROOMCAP_PRE))
-        temp = list(room_graph.objects(subject=subject, predicate=URIRef(AVL_PRE)))
+        room_cap = room_graph.value(
+            subject=subject, predicate=URIRef(EX.roomCapacity))
+        temp = list(room_graph.objects(
+            subject=subject, predicate=URIRef(EX.hasAvailability)))
         available_slots = []
         for p in temp:
             _, slot = split_uri(p)
             slot = slot[1:]
             available_slots.append(slot)
-        room_caps_and_availability[class_room] = (int(room_cap.toPython()), available_slots)
+        min_room_caps_and_availability[class_room] = (
+            int(room_cap.toPython()), available_slots)
 
-for r in room_caps_and_availability.items():
+for r in min_room_caps_and_availability.items():
     print()
     print(r)
 
@@ -72,13 +73,13 @@ for r in room_caps_and_availability.items():
 #     print(s, p, o)
 
 
-## define the slots
+# define the slots
 for subject in room_graph.subjects():
-    if subject.startswith(SLOTPRE):
+    if subject.startswith(EX._Time_slot):
         _, slot = split_uri(subject)
         slot = slot[1:]
-        start = room_graph.value(subject=subject, predicate=URIRef(STARTPRE))
-        end = room_graph.value(subject=subject, predicate=URIRef(ENDPRE))
+        start = room_graph.value(subject=subject, predicate=URIRef(EX.availableFrom))
+        end = room_graph.value(subject=subject, predicate=URIRef(EX.availableUntil))
 
         slot_length = end.toPython() - start.toPython()
         slot_hours = slot_length.total_seconds() / 3600.0
@@ -90,47 +91,52 @@ for item in time_slot_defs.items():
     print()
     print(item)
 
-# for s, p, o in room_graph:
-#     print(s, p, o)
+# sliding window
+def all_exam_windows(slot_start, slot_end, exam_duration, step_hours=1):
+    windows = []
+    current = slot_start
 
-potentials = {}
-candidates = set()
+    while current + timedelta(hours=exam_duration) <= slot_end:
+        end = current + timedelta(hours=exam_duration)
+        windows.append((current, end))
+        current += timedelta(hours=step_hours)
+    
+    return windows
+        
 
-# def schedule_backtracking(key: str) -> dict:
+potential_rooms = {}
 
 # narrow down potential rooms by capacity
+# def schedule_backtracking(key: str) -> dict:
 for c in classes:
-    min_cap = classes[c][0]
-    for room in room_caps_and_availability:
-        max_room_cap = room_caps_and_availability[room][0]
-        if max_room_cap < min_cap:
+    min_cap, exam_duration = classes[c]
+    candidates = set()
+    for room, (room_cap, _) in min_room_caps_and_availability.items():
+        if room_cap >= min_cap:
             candidates.add(room)
-        potentials[c] = candidates
-    # check if each room has a timeslot long enough for exam / exam_duration <= time_slot.length
-    for room in potentials[c]:
-        pass
+    potential_rooms[c] = candidates
 
-    # translate piece of time_slot taken up into real time / e.g. if total_time = 10.0 (12:00pm - 10:00pm) 
-    # and exam_duration = 2.0 (12:00pm - 2:00pm), total_time now = 8.0 and 12-2 of slot_x = taken  
+    for room in potential_rooms[c]:
+        for slot, (slot_start, slot_end, slot_hours) in time_slot_defs.items():
+           possible_times = all_exam_windows(
+               slot_start,
+               slot_end,
+               exam_duration,
+               step_hours=1
+           )
+
+           for start, end in possible_times:
+                print(f"Course: {c}, Exam Duration: {exam_duration}/hrs, Potential Room: {room}, Time Slot: {slot}, Total Slot Length: {slot_hours}/hrs, Possible Exam Time: ({start} to {end})")
+    
+    
+    # make sure possible exam time is not already taken
+    
+    reserved = {}
+
 
     # check for student conflicts (a student has two exam simultaneously)
 
-    # check for double booked rooms
-
-    
     
 
-
-
-
-
-print()
-
-for p in potentials.items():
-    print(p)
-
-
-
-
-
-
+    # course -> exam time
+    # exam time -> all students in course
